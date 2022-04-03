9348b75e02a103b58aef96c7db533560e180e858
Harshit Jain <mrharshitjain1@gmail.com>
2022-04-03 18:17:24 +0530

    finished

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ var uppercase = document.getElementById('uppercase');
 var lowercase = document.getElementById('lowercase');
 var output = document.getElementById('passwordShow');
 
+if (localStorage.getItem("passwordrange")){
+    var past= localStorage.getItem("passwordrange")
+    PasswordRange.value = past ;
+
+}
+
 
 function generatePassword() {
+    n = PasswordRange.value;
+
+    // Seting the Select Password Range
+    localStorage.setItem("passwordrange",n);
     var isChecked = document.getElementsByName('password');
     const characters = [];
     for (var i = 0; i < isChecked.length; i++) {
@@ -16,14 +26,18 @@ function generatePassword() {
     }
     const stringChar = characters.toString();
     console.log(stringChar);
-    n = PasswordRange.value;
     let password = '';
     for ( let i = 0; i<n; i++ ) {
         password += stringChar.charAt(Math.floor(Math.random() * stringChar.length));
     }
     output.value = password;
+    output.select();
     console.log(password);
     }
+
+function copy(){
+    navigator.clipboard.writeText(output.value);
+}
     
 
 
