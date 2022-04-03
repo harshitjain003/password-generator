4448a830089539794ff0c80c601762f64afec436
Harshit Jain <mrharshitjain1@gmail.com>
2022-04-03 17:08:52 +0530

    changes done

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-p = document.getElementById("id");
-function getUrl(){
-    const n = 10;
-    const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var randomString = '';
+var PasswordRange = document.getElementById('passwordRange');
+var symbols = document.getElementById('symbols');
+var numbers = document.getElementById('numbers');
+var uppercase = document.getElementById('uppercase');
+var lowercase = document.getElementById('lowercase');
+var output = document.getElementById('passwordShow');
 
-    for (i = 0; i < n; i++) {
-        var index = Math.random(0, (characters.length) - 1);
-        randomString = characters[index];
+var isChecked = document.getElementsByName('password');
+var Values = document.getElementsByClassName('values').value;
+var characters = [];
+for (var i=0; i<isChecked.length; i++) {
+    if (isChecked[i].checked) {
+        
+        characters.push(Values[i].value);
+        console.log(characters);
     }
-    console.log(randomString);
-    p.innerText = randomString;
 }
+
+
+function generatePassword(){
+    n = PasswordRange.value;
+
+    let password = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < n; i++ ) {
+        password += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return password;
+}
+    console.log(index);
+    output.value = generatePassword();
+
 
